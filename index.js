e2025a066d745bc49acd4ba3b53364e94b5cf742
Suhad Mia <suhadmia12@gmail.com>
2023-06-07 17:38:13 +0600

    add toy and my toy server done

--- index.js
+++ index.js
@@ -22,14 +22,121 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+
+      const toysCollection = client.db('babyCarsToy').collection('allToys');
+
+
+
+      app.get("/allToy", async (req, res) => {
+          const result = await toysCollection.estimatedDocumentCount();
+          res.send({ result });
+      });
+
+      app.get("/allToys", async (req, res) => {
+          const data = req.query;
+          const category = req.query?.category;
+          const search = req.query.search;
+
+          let query = {};
+
+          if (category) {
+              query = { sub_category: category };
+          }
+
+          if (search) {
+              query = { title: { $regex: search, $options: 'i' } }
+          }
+
+
+          // const page = parseInt(data.page) || 0;
+          // const limit = parseInt(data.limit) || 20;
+          // const skip = page * limit;
+
+          const result = await toysCollection
+              .find(query)
+              .toArray();
+          res.send(result);
+      });
+
+      app.get('/toy/:id', async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) };
+          const result = await toysCollection.findOne(query);
+          res.send(result)
+      })
+
+
+      app.get('/myToys', async (req, res) => {
+          const email = req.query?.email;
+          const sortData = req.query.sort;
+
+          let query = {}
+
+          if (email) {
+              query = { email: email }
+          }
+
+          let sortQuery = {}
+
+          if (sortData) {
+              if (sortData === "asc") {
+                  sortQuery = { price: 1 };
+              } else if (sortData === "desc") {
+                  sortQuery = { price: -1 };
+              }
+          }
+
+          const result = await toysCollection.find(query).sort(sortQuery).toArray();
+          res.send(result);
+      })
+
+
+      app.post('/allToys', async (req, res) => {
+          const addToy = req.body;
+          const result = await toysCollection.insertOne(addToy);
+          res.send(result);
+      });
+
+
+      app.put('/myToys/:id', async (req, res) => {
+          const id = req.params.id;
+          const toy = req.body;
+
+          const filter = { _id: new ObjectId(id) }
+
+          const options = { upsert: true }
+
+          const updatedToy = {
+              $set: {
+                  title: toy?.title,
+                  photoUrl: toy?.photoUrl,
+                  description: toy?.description,
+                  price: toy?.price,
+                  quantity: toy?.quantity,
+                  rating: toy?.rating,
+                  sub_category: toy?.sub_category,
+              }
+          }
+
+          const result = await toysCollection.updateOne(filter, updatedToy, options)
+
+          res.send(result)
+      })
+
+      app.delete('/myToys/:id', async (req, res) => {
+          const id = req.params.id;
+          const filter = { _id: new ObjectId(id) }
+          const result = await toysCollection.deleteOne(filter);
+          res.send(result)
+      })
+
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+      // Ensures that the client will close when you finish/error
+      // await client.close();
   }
 }
 run().catch(console.dir);
